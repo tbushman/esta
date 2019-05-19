c9e38b8b7abd53d84fa4ac2c11ad2dc8cb706532
tbushman <tbushman@pu.bli.sh>
2019-05-19 02:39:06 -0600

    run middleware

--- routes/index.js
+++ routes/index.js
@@ -1434,7 +1434,7 @@ router.get('/logout', function(req, res, next) {
 	})
 })*/
 
-router.get('/profile/:username', ensureSigPorted,  function(req, res, next) {
+router.get('/profile/:username',  function(req, res, next) {
 	Content.find({}).sort({'properties.time.end': 1}).lean().exec(function(err, data){
 		if (err) {return next(err)}
 		Publisher.findOne({_id: req.session.userId}, function(err, pu){
@@ -1802,7 +1802,7 @@ router.post('/sig/uploadsignature/:did/:puid', ifExistsReturn, uploadmedia.singl
 	})
 });
 
-router.get('/sig/editprofile', function(req, res, next){
+router.get('/sig/editprofile', ensureSigPorted, function(req, res, next){
 // console.log('bleh')
 	Content.find({}).lean().sort({'properties.time.end': 1}).exec(function(err, data){
 		if (err) {return next(err)}
